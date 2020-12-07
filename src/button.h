d256e3adaa1e950650d804c313b08241f87fe244
Dykhova Anna <dyhovaanna7@gmail.com>
2020-12-07 15:47:08 +0700

    a

---
+++ src/button.h
@@ -0,0 +1,6 @@
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
+#include "type_custom.h"
+void button_Init(void);
+uint8_t button_State(void);
+#endif
---
+++ src/type_custom.h
@@ -0,0 +1,8 @@
+#ifndef __TYPE_CUSTOM_H__
+#define __TYPE_CUSTOM_H__
+#include "MDR32F9Qx_config.h"
+#include "MDR32F9Qx_board.h"
+#include "MDR32F9Qx_rst_clk.h"
+#include "MDR32F9Qx_port.h"
+#include "stdint.h"
+#endif
